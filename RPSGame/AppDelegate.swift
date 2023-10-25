d73001c6278dbd21b7b8d854ac95dda715c50a56
Hyeonhee <hyunheeshim0103@gmail.com>
2023-10-26 00:30:08 +0900

    Initial Commit

---
+++ RPSGame/AppDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  AppDelegate.swift
+//  RPSGame
+//
+//  Created by 심현희 on 2023/09/16.
+//
+
+import UIKit
+// main.swift 파일과 동일한 역할을 한다는 표시 @main 해당파일이 가장먼저 실행된다 
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ RPSGame/Rps.swift
@@ -0,0 +1,15 @@
+//
+//  Rps.swift
+//  RPSGame
+//
+//  Created by 심현희 on 2023/09/17.
+//
+
+import Foundation
+
+// 원시값(raw value)설정
+enum Rps: Int {
+    case rock //0
+    case paper //1
+    case scissors //2
+}
